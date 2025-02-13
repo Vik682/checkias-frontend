c175ce4e8b8cbb7b99c95123ae2f592bcf910385
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2025-02-13 08:21:43 +0530

    changes

--- components/Admin/Payment_Request/Payment_Request.tsx
+++ components/Admin/Payment_Request/Payment_Request.tsx
@@ -86,7 +86,7 @@ const PaymentRequest = () => {
   };
 
   return (
-    <div className="ml-48 p-6">
+    <div >
       {/* Filter and Search Bar at the top right */}
       <div className="flex justify-between items-center mb-4">
         <div className="flex items-center">
